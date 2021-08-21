021238449e401b008e1ccf99efd4a82aecf63c2f
Santosh <realsantosm@gmail.com>
2021-08-21 11:28:30 +0530

    style : styling angle page

---
+++ angletri.js
@@ -0,0 +1,28 @@
+const input = document.querySelectorAll('.inputBox');
+const formTriangle = document.querySelector('#form-Triangle');
+const outputHere = document.querySelector('.output-text');
+const submitBtn = document.querySelector('#submit-btn');
+
+let angles=[];
+
+document.addEventListener('click', clickHandler);
+
+function clickHandler(e){
+    e.preventDefault();
+
+    for(i=0; i < input.length; i++){
+        angles[i]=Number(input[i].value);
+    }
+
+    let sum = 0;
+    angles.map((angle)=>{
+        sum = sum + angle;
+    });
+
+    if(sum === 180){
+        outputHere.innerText = "Hurray! Your angles can make triangle...! <br> 🤩";
+    } else {
+        outputHere.innerText = "Alas! Your angles could not form Triangle...! Your total sum of angles should be 180. 🤷‍♂️";
+    }
+    
+}
