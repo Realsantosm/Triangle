16869e12b57c47797853bd673072640c3bd15c91
Santosh <realsantosm@gmail.com>
2021-08-28 14:30:18 +0530

    fix : connecting the html with js file

---
+++ anglearea.js
@@ -0,0 +1,5 @@
+//JavaScript for the Area of a Triangle !!!!!!
+
+const areaInputs = document.querySelectorAll('.area-inputs');
+const areaBtn = document.querySelector('#area-btn');
+const areaOutput = document.querySelector('#area-output');
