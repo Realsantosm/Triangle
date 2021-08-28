7c48c4f2f84ced5d5427b5ad5fb2e72620fc7799
Santosh <realsantosm@gmail.com>
2021-08-28 14:45:32 +0530

    Fix : Js statement fixed

--- anglearea.js
+++ anglearea.js
@@ -4,19 +4,20 @@ const areaInputs = document.querySelectorAll('.area-inputs');
 const areaBtn = document.querySelector('#area-btn');
 const areaOutput = document.querySelector('#area-output');
 
-
-
-areaBtn.addEventListener('click', calculateArea);
+function baseIntoHeight(base,height){
+    const multiplied = base * height;
+    return multiplied;
+}
 
 function calculateArea(){
-    const baseIntoHeight = multiplyBaseAnd innerHeight(areaInputs[0].nodeValue, areaInputs[1].value){
+    const baseIntoHeight = multiplyBaseAndHeight.innerHeight(areaInputs[0].nodeValue, areaInputs[1].value){
         const area = baseIntoHeight / 2;
         console.log({area});
         areaOutput.innerText = 'The aArea of a Triangle is ${area} cm^2'
     }
 }
 
-function baseIntoHeight(base,height){
-    const multiplied = base * height;
-    return multiplied;
-}
+areaBtn.addEventListener('click', calculateArea);
+
+
+
